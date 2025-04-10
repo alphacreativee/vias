27cd8650f3822925522970afdfc45ce877732eb8
HuuBang11 <huubang0312@gmail.com>
2025-04-10 18:13:36 +0700

    update animation

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -2,10 +2,27 @@ import { preloadImages } from "../../libs/utils.js";
 let lenis;
 Splitting();
 
+function header() {
+  ScrollTrigger.refresh();
+
+  ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+      self.progress === 0 ? $("header").removeClass("header--scroll") : "";
+      // self.direction === 1 ? $("header").addClass("header--scroll") : "";
+
+      if (self.progress > 0.006) {
+        $("header").addClass("header--scroll");
+      } else {
+        $("header").removeClass("header--scroll");
+      }
+    }
+  });
+}
+
 function discover() {
   if ($(".section-discover").length < 1) return;
-
-  console.log("true");
 
   var swiperDiscover = new Swiper(".swiper-discover", {
     slidesPerView: 2,
@@ -21,9 +38,98 @@ function discover() {
   });
 }
 
+function customDropdown() {
+  const $dropdowns = $(".dropdown-custom");
+
+  $dropdowns.each(function () {
+    const $dropdown = $(this);
+    const $btnDropdown = $dropdown.find(".dropdown-custom__btn");
+    const $dropdownMenu = $dropdown.find(".dropdown-custom__menu");
+    const $dropdownItems = $dropdown.find(".dropdown-custom__item");
+    const $textDropdown = $dropdown.find(".dropdown-custom__text");
+
+    $btnDropdown.on("click", function (e) {
+      e.stopPropagation();
+      closeAllDropdowns($dropdown);
+      $dropdownMenu.toggleClass("dropdown--active");
+      $btnDropdown.toggleClass("--active");
+    });
+
+    $(document).on("click", function () {
+      closeAllDropdowns();
+    });
+
+    $dropdownItems.on("click", function (e) {
+      e.stopPropagation();
+      const $item = $(this);
+      let tmpText = $textDropdown.text();
+      const tmpImgSrc = $textDropdown.find("img").attr("src"); // Get the current image src if present
+      const $img = $item.find("img"); // Check if the clicked item contains an img
+
+      // Swap text content
+      $textDropdown.text($item.text());
+
+      // If the item has an image, swap the img src
+      if ($img.length) {
+        $textDropdown.html($item.html()); // Swap the entire HTML, including the img
+
+        if ($item.hasClass("language__item")) {
+          tmpText = `<span>${tmpText}</span>`;
+        }
+
+        $item.html(
+          `${tmpImgSrc ? `<img src="${tmpImgSrc}" />` : ""} ${tmpText}`
+        ); // Swap img and text back to the item
+      } else if ($item.hasClass("language__item")) {
+        $item.text(tmpText);
+      }
+
+      closeAllDropdowns();
+    });
+
+    function closeAllDropdowns(exception) {
+      $(".dropdown-custom__btn").removeClass("active");
+      $dropdowns.each(function () {
+        const $menu = $(this).find(".dropdown-custom__menu");
+        const $ic = $(this).find(".dropdown-custom__btn");
+        if (!exception || !$(this).is(exception)) {
+          $menu.removeClass("dropdown--active");
+          $ic.removeClass("--active");
+        }
+      });
+    }
+  });
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".data-fade-in").forEach((element, i) => {
+  gsap.fromTo(
+    element,
+    {
+      opacity: 0,
+      y: 20
+    },
+    {
+      scrollTrigger: {
+        trigger: element,
+        start: "top 70%",
+        end: "bottom 70%"
+      },
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power1.out",
+      stagger: 0.1
+    }
+  );
+});
+
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
+  header();
   discover();
+  customDropdown();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
