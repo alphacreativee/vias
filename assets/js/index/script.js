ff453be5d5fd26d8313a23bc32b96f41306a345d
HuuBang11 <huubang0312@gmail.com>
2025-04-10 14:13:09 +0700

    update code

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -2,8 +2,28 @@ import { preloadImages } from "../../libs/utils.js";
 let lenis;
 Splitting();
 
+function discover() {
+  if ($(".section-discover").length < 1) return;
+
+  console.log("true");
+
+  var swiperDiscover = new Swiper(".swiper-discover", {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    loop: true,
+    speed: 1500,
+    spaceBetween: 24,
+    loopAdditionalSlides: 8,
+    navigation: {
+      nextEl: ".swiper-discover .swiper-button-next",
+      prevEl: ".swiper-discover.swiper-button-prev"
+    }
+  });
+}
+
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
+  discover();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
